7ce0253a264cb2de2660ea2f29de9f1a1533db36
quluk <143124115+quluk@users.noreply.github.com>
2024-04-16 22:55:16 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -8,7 +8,38 @@ public class Digits {
         digitList = new ArrayList<>();
         while (num > 0) {
             digitList.add(0, num % 10);
-            num /= 10;
+            num /= 10 import java.util.ArrayList;
+
+public class Digits
+{
+
+	private ArrayList<Integer> digitList;
+
+	public Digits(int num)
+	{ 
+	   for(int i =0; i<(""+num).length();i++){
+		digitList.set(i,num%10);
+		   num=num/10;
+	   }
+	}
+
+	public boolean isStrictlyIncreasing()
+	{ 
+	 int max = -1;
+		for( int i =0; i<digitList.size();i++){
+			if(max<digitList.get(i)){
+				max=digitList.get(i);
+			} else{
+				return false; }
+		}
+		return true;
+	}
+	
+	public String toString()
+	{
+		return digitList.toString();
+	}
+};
         }
     }
 
